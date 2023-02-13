896e0fac55d506028b4b67b436f689576a30a243
ConanGoodwin <conan_goodwin@hotmail.com>
2023-02-13 15:24:20 -0300

    add car model

---
+++ src/Models/CarODM.ts
@@ -0,0 +1,20 @@
+import { model, Model, models, Schema } from 'mongoose';
+import ICar from '../Interfaces/Car.interface';
+
+export default class CarODM {
+  private _schema: Schema;
+  private _model: Model<ICar>;
+
+  constructor() {
+    this._schema = new Schema<ICar>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: false },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+    this._model = models.Car || model('Car', this._schema);
+  }
+}
