34f6568545ea82b077704b7b6baf60df1c0c5034
ConanGoodwin <conan_goodwin@hotmail.com>
2023-02-14 16:41:37 -0300

    add testes de find um carro em especifico

--- tests/unit/Services/car.service.test.ts
+++ tests/unit/Services/car.service.test.ts
@@ -76,29 +76,60 @@ describe('Testes do serviço Car', function () {
     });
   });
 
-  describe('Testes relativos a busca de Car', function () {
-    describe('Testa as buscas de todos os carros', function () {
-      it('Testa se os carro novo são retornados com sucesso', async function () {
-        sinon.stub(Model, 'find').resolves(allCar);
-    
-        const { type, message } = await new CarService().findAll();
-    
-        expect(type).to.be.equal(null);
-        expect(message).to.be.deep.equal(allCar);
-      });
+  describe('Testa as buscas de todos os carros', function () {
+    it('Testa se todos os carros são retornados com sucesso', async function () {
+      sinon.stub(Model, 'find').resolves(allCar);
   
-      it('Testa erro ao criar uma carro', async function () {
-        sinon.stub(Model, 'find').resolves(undefined);
-    
-        const { type, message } = await new CarService().findAll();
-    
-        expect(type).to.be.equal('erro');
-        expect(message).to.be.equal('error');
-      });
+      const { type, message } = await new CarService().findAll();
   
-      afterEach(function () {
-        sinon.restore();
-      });
+      expect(type).to.be.equal(null);
+      expect(message).to.be.deep.equal(allCar);
+    });
+
+    it('Testa erro buscar todos os carros', async function () {
+      sinon.stub(Model, 'find').resolves(undefined);
+  
+      const { type, message } = await new CarService().findAll();
+  
+      expect(type).to.be.equal('erro');
+      expect(message).to.be.equal('error');
+    });
+
+    afterEach(function () {
+      sinon.restore();
+    });
+  });
+
+  describe('Testa as buscas um carro especifico', function () {
+    it('Testa se um carro é retornado com sucesso', async function () {
+      sinon.stub(Model, 'findById').resolves(carOutPut);
+  
+      const { type, message } = await new CarService().findById('63ebdb90f4e0ac2072dd9bff');
+  
+      expect(type).to.be.equal(null);
+      expect(message).to.be.deep.equal(carOutPut);
+    });
+
+    it('Testa erro ao enviar um id no formato inválido', async function () {
+      sinon.stub(Model, 'findById').resolves(undefined);
+  
+      const { type, message } = await new CarService().findById('63ebd');
+  
+      expect(type).to.be.equal('INVALID');
+      expect(message).to.be.equal('Invalid mongo id');
+    });
+
+    it('Testa erro ao não encontrar o id', async function () {
+      sinon.stub(Model, 'findById').resolves(undefined);
+  
+      const { type, message } = await new CarService().findById('63ebdb90f4e0ac2072dd9bff');
+  
+      expect(type).to.be.equal('NOT_FOUND');
+      expect(message).to.be.equal('Car not found');
+    });
+
+    afterEach(function () {
+      sinon.restore();
     });
   });
 });
