5608d2c2288f879f2ec8a60927b6a4d0af010823
ConanGoodwin <conan_goodwin@hotmail.com>
2023-02-14 16:30:18 -0300

    add teste de find de todos Car

--- tests/unit/Services/car.service.test.ts
+++ tests/unit/Services/car.service.test.ts
@@ -28,6 +28,29 @@ const carOutPut = {
   id: '63ebdb90f4e0ac2072dd9bff',
 };
 
+const allCar = [
+  {
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: false,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+    id: '63ebdb90f4e0ac2072dd9bff',
+  },
+  {
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: false,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+    id: '63ebdb90f4e0ac2072dd9bff',
+  },
+];
+
 describe('Testes do serviço Car', function () {
   describe('Testes relativos a criação de um novo Car', function () {
     it('Testa se um carro novo é criado com sucesso', async function () {
@@ -50,6 +73,32 @@ describe('Testes do serviço Car', function () {
 
     afterEach(function () {
       sinon.restore();
+    });
+  });
+
+  describe('Testes relativos a busca de Car', function () {
+    describe('Testa as buscas de todos os carros', function () {
+      it('Testa se os carro novo são retornados com sucesso', async function () {
+        sinon.stub(Model, 'find').resolves(allCar);
+    
+        const { type, message } = await new CarService().findAll();
+    
+        expect(type).to.be.equal(null);
+        expect(message).to.be.deep.equal(allCar);
+      });
+  
+      it('Testa erro ao criar uma carro', async function () {
+        sinon.stub(Model, 'find').resolves(undefined);
+    
+        const { type, message } = await new CarService().findAll();
+    
+        expect(type).to.be.equal('erro');
+        expect(message).to.be.equal('error');
+      });
+  
+      afterEach(function () {
+        sinon.restore();
+      });
     });
   });
 });
